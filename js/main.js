7f4ae969c4f4089947c347a88ffc1c877b79010e
juan <mtzn_juandaniel@yahoo.com.mx>
2020-05-12 18:50:41 -0600

    routes problem

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function featureTab(tab) {
     switch (tab.id) {
         case 'featItem01':
             document.querySelector('.description__background__circle').style.width = '42%';
-            featBackground.setAttribute('src', '../images/illustration-features-tab-1.svg');
+            featBackground.setAttribute('src', './images/illustration-features-tab-1.svg');
             featTitle.innerHTML = 'Bookmark in one click';
             featDescription.innerHTML = 'Organize your bookmarks however you like.Our simple drag - and - drop interface gives you complete control over how you manage your favourite sites.';
 
@@ -60,7 +60,7 @@ function featureTab(tab) {
         case 'featItem02':
             document.querySelector('.description__background__circle').style.width = '34%';
             document.querySelector('.description__background__circle').style.transform = 'translateY(-90%)';
-            featBackground.setAttribute('src', '../images/illustration-features-tab-2.svg');
+            featBackground.setAttribute('src', './images/illustration-features-tab-2.svg');
             featTitle.innerHTML = 'Intelligent search';
             featDescription.innerHTML = 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.';
 
@@ -69,7 +69,7 @@ function featureTab(tab) {
         case 'featItem03':
             document.querySelector('.description__background__circle').style.width = '34%';
             document.querySelector('.description__background__circle').style.transform = 'translateY(-90%)';
-            featBackground.setAttribute('src', '../images/illustration-features-tab-3.svg');
+            featBackground.setAttribute('src', './images/illustration-features-tab-3.svg');
             featTitle.innerHTML = 'Share your bookmarks';
             featDescription.innerHTML = 'Easily share your bookmarks and collections with others.Create a shareable link that you can send at the click of a button.';
 
@@ -87,10 +87,10 @@ function showAnswer(tab) {
     document.querySelector('#dItem0' + tab.id).classList.toggle('hidden');
 
     if (document.querySelector('#dItem0' + tab.id).classList.contains('hidden')) {
-        document.querySelector('#arrow0' + tab.id).src = '../images/icon-arrow.svg';
+        document.querySelector('#arrow0' + tab.id).src = './images/icon-arrow.svg';
         document.querySelector('#arrow0' + tab.id).style.transform = 'rotate(0deg)';
     } else {
-        document.querySelector('#arrow0' + tab.id).src = '../images/icon-arrow-up.svg';
+        document.querySelector('#arrow0' + tab.id).src = './images/icon-arrow-up.svg';
         document.querySelector('#arrow0' + tab.id).style.transform = 'rotate(180deg)';
     }
 }
