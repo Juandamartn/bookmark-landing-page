33c92aac77df050619a25536633b4dafef94870f
juan <mtzn_juandaniel@yahoo.com.mx>
2020-05-12 18:33:18 -0600

    Responsive issues and hovers

--- js/main.js
+++ js/main.js
@@ -18,7 +18,12 @@ var featBackground = document.getElementById('description__background__image'),
 if (window.screen.width <= 768) {
     headerMenu.classList.toggle('hidden');
     headerHamburger.classList.toggle('hidden');
-    featuresButton.classList.toggle('hidden');
+    document.querySelector('.header__nav__mobile').classList.toggle('hidden');
+    document.querySelector('.header__nav__social').classList.toggle('hidden');
+}
+
+if (window.screen.width > 768) {
+    document.querySelector('#features__more-info').classList.toggle('hidden');
 }
 
 headerHamburger.addEventListener('click', function () {
@@ -45,6 +50,7 @@ function featureTab(tab) {
 
     switch (tab.id) {
         case 'featItem01':
+            document.querySelector('.description__background__circle').style.width = '42%';
             featBackground.setAttribute('src', '../images/illustration-features-tab-1.svg');
             featTitle.innerHTML = 'Bookmark in one click';
             featDescription.innerHTML = 'Organize your bookmarks however you like.Our simple drag - and - drop interface gives you complete control over how you manage your favourite sites.';
@@ -52,6 +58,8 @@ function featureTab(tab) {
             break;
 
         case 'featItem02':
+            document.querySelector('.description__background__circle').style.width = '34%';
+            document.querySelector('.description__background__circle').style.transform = 'translateY(-90%)';
             featBackground.setAttribute('src', '../images/illustration-features-tab-2.svg');
             featTitle.innerHTML = 'Intelligent search';
             featDescription.innerHTML = 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.';
@@ -59,6 +67,8 @@ function featureTab(tab) {
             break;
 
         case 'featItem03':
+            document.querySelector('.description__background__circle').style.width = '34%';
+            document.querySelector('.description__background__circle').style.transform = 'translateY(-90%)';
             featBackground.setAttribute('src', '../images/illustration-features-tab-3.svg');
             featTitle.innerHTML = 'Share your bookmarks';
             featDescription.innerHTML = 'Easily share your bookmarks and collections with others.Create a shareable link that you can send at the click of a button.';
@@ -86,7 +96,7 @@ function showAnswer(tab) {
 }
 
 function closeMenu() {
-    if (window.screen.width <= 425) {
+    if (window.screen.width <= 768) {
         headerMenu.classList.toggle('hidden');
     }
 }
